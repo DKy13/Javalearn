2ec754c9e077c1892fbbc570e5eb13b2dffc32e2
github's Name <3011965212@qq.com>
2020-06-10 21:41:00 +0800

    求最小公倍数

---
+++ Test0610.java
@@ -0,0 +1,24 @@
+package learn;
+
+import java.util.Scanner;
+
+public class Test0610 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int A=sc.nextInt();
+            int B=sc.nextInt();
+            int result=A*B;
+            result=result/getYinShu(A,B);
+            System.out.println(result);
+        }
+    }
+    public static int getYinShu(int m,int n){
+        int i=m;
+        while((m%i!=0)||(n%i!=0)){
+            i--;
+        }
+        return i;
+    }
+}
